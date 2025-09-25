10afc7dd070e582694fc4535dd8b20183618402b
Sissy Haegele <sissy.haegele@web.de>
2025-09-25 18:50:20 +0200

    Final project structure - clean and optimized

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+﻿self.addEventListener('install', (event) => {
+  console.log('Service Worker installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
+});
+
+self.addEventListener('fetch', (event) => {
+  // Einfacher Pass-through für jetzt
+  event.respondWith(fetch(event.request));
+});
